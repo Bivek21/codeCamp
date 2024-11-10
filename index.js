304a3abb0552738c39ff8e30e8c6164cc59506c1
Bivek Baral <baralbibek1234@gmail.com>
2024-11-10 09:58:41 +0930

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+//document.getElementById("count-el").innerText = 5;
+// let firstBatch = 5;
+// let secondBatch = 7;
+// let count = firstBatch + secondBatch;
+// console.log(count);
+// let myAge = 29;
+// let humanDogRatio = 7;
+// let myDogAge = myAge * humanDogRatio;
+// console.log(myDogAge);
+
+let count = 5;
+//count +1
+count = count + 1;
+count = count - 2;
+console.log(count);
